46879c3977630b99604ef593040f27a1f39a4bb8
ChefDK User <chef@ip-172-31-58-162.ec2.internal>
2019-05-07 22:26:59 +0000

    changed from blue to red

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -1,7 +1,7 @@
 pkg_name=national-parks
 pkg_description="A sample JavaEE Web app deployed in the Tomcat8 package"
 pkg_origin=eh-chefconf
-pkg_version=1.0.1
+pkg_version=1.0.2
 pkg_maintainer="Bill Meyer <b@chef.io>"
 pkg_license=('Apache-2.0')
 pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
